5ee38688f93754240d5b1896e42607aa02da0e13
Jorge González <iksaku@me.com>
2019-09-05 03:39:49 -0500

    Testing again...

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,10 @@
 
 swift auth &>/dev/null
 
-if [ $? -eq 0 ]; then
-    echo "Successfully authenticated"
-else
+if [ $? -ne 0 ]; then
     echo "[Error] Unable to authenticate against Swift provider."
     echo -e "\tPlease ensure all your keystone variables are set and check your credentials."
     exit 1
 fi
+
+sh -c "swift $*"
